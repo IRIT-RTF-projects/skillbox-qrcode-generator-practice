839940724a5b12446bbb23477550c27ca9def3bd
vlad-crab <vladikazakov05@gmail.com>
2025-05-19 23:26:48 +0500

    m

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,49 @@
-async function main() {
-    console.log("Hello, world!")
+import QRCode from "qrcode";
+
+
+type ParsedArgs = {
+    text: string,
+    size?: number
 }
 
-if (typeof require !== 'undefined' && require.main === module) {
-    main()
+function parseArgv(): ParsedArgs {
+    const { argv } = process
+
+    if (argv[2] != 'generate') throw new Error('unknown command')
+    const text = argv[3]
+    if (text == null || text == '') throw new Error('please provide text')
+
+    if (argv[4] == null) return { text }
+
+    if (argv[4] !== '--size') throw new Error('unknown parameter: ' + argv[4])
+
+    const size = Number(argv[5])
+    if (Number.isNaN(size) || size < 1 || size > 40) throw new Error('bad size param, needs to be a number beetween 1 and 40')
+
+    return { text, size }
 }
+
+
+async function generateQr(text: string, size: number | undefined) {
+    try {
+        console.log(await QRCode.toString(
+            text,
+            { version: size }
+        ))
+    } catch (err) {
+        if (err instanceof Error) {
+            console.log(err.message)
+        }
+    }
+}
+
+async function main() {
+    try {
+        const { text, size } = parseArgv()
+        await generateQr(text, size)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+main()
--- test/willFail.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, test, expect } from "vitest"
-
-
-describe('test you wrote some tests', () => {
-    test('', () => {
-        expect(true).toBe(false)
-    })
-})
